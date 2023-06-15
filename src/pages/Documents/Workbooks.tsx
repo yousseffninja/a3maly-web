25b7ec4f0370adf866c6c31c886e4b84e7a98d67
backm <yousseffmohamedd22@gmail.com>
2023-06-15 11:23:01 +0300

    Complete AI-image
    Complete Workbook

--- src/pages/Documents/Workbooks.tsx
+++ src/pages/Documents/Workbooks.tsx
@@ -5,13 +5,35 @@ import { useTranslation } from 'react-i18next';
 import {
   Box,
   Container,
-  Grid,
   Stack,
-  Typography
+  Typography,
+  Card,
+  Divider,
+  Grid,
+  MenuItem,
+  Select,
+  Button,
+  SvgIcon
 } from '@mui/material';
+import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
+import { OfficesTable } from '@/sections/offices/offices-table';
 const Page = () => {
   const {t}= useTranslation();
   const title = "Workbooks";
+  const Artist = [
+    {
+      id: 1,
+      name: "Artist 1"
+    },
+    {
+      id: 2,
+      name: "Artist 2"
+    },
+    {
+      id: 3,
+      name: "Artist 3"
+    }
+  ]
   return (
     <>
       <Head>
@@ -33,6 +55,78 @@ const Page = () => {
                 <Typography variant="h4">{t(title)}</Typography>
               </Stack>
             </Stack>
+            <Card sx={{ px: 2, py: 2 }}>
+              <Typography sx={{ my : 1}} variant="h6">{t("All My Workbooks")}</Typography>
+              <Divider sx={{ my : 1}}/>
+              <Grid spacing={2} sx={{ my : 1, display: "flex"}}>
+                <Grid sx={{width: "50%"}}>
+                  <Select
+                    value={Artist[0]?.id}
+                    required={true}
+                    onChange={() => null}
+                    inputProps={{ "aria-label": "Without label" }}
+                    sx={{
+                      height: 'auto',
+                      maxHeight: 100,
+                      overflowY: 'auto',
+                      width: "100%"
+                    }
+                    }
+                  >
+                    {Artist &&
+                      Artist?.map((art: any) => (
+                        <MenuItem key={art?.id} value={art?.id} >
+                          {art?.name}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </Grid>
+                <Grid direction="row" sx={{my: 1, mx: 2}}>
+                  <Button
+                    startIcon={
+                      <SvgIcon fontSize="small">
+                        <PlusIcon />
+                      </SvgIcon>
+                    }
+                    sx={{ pr: 1, mx: 1 }}
+                    variant="contained"
+                  />
+                  <Button
+                    startIcon={
+                      <SvgIcon fontSize="small">
+                        <PlusIcon />
+                      </SvgIcon>
+                    }
+                    sx={{ pr: 1, mx: 1 }}
+                    variant="contained"
+                  />
+                  <Button
+                    startIcon={
+                      <SvgIcon fontSize="small">
+                        <PlusIcon />
+                      </SvgIcon>
+                    }
+                    sx={{ pr: 1, mx: 1 }}
+                    variant="contained"
+                  />
+                </Grid>
+              </Grid>
+            </Card>
+            <Card sx={{ width: "100%" }}>
+              <OfficesTable
+                count={0}
+                items={[]}
+                onDeselectAll={() => null}
+                onDeselectOne={() => null}
+                onPageChange={() => null}
+                onRowsPerPageChange={() => null}
+                onSelectAll={() => null}
+                onSelectOne={() => null}
+                page={1}
+                rowsPerPage={1}
+                selected={[]}
+              />
+            </Card>
           </Stack>
         </Container>
       </Box>
